90be81090080d2e1b08daecd7dd674aa43ae5abb
UselessBen1 <66045986+UselessBen1@users.noreply.github.com>
2023-10-15 23:22:48 -0400

    card with enum, rank,colour,special

---
+++ UnoFlip/src/Card.java
@@ -0,0 +1,35 @@
+public class Card {
+    private Rank rank;
+    private Colour colour;
+    private Special special;
+    public enum Colour{RED,YELLOW,BLUE,GREEN,BLACK}
+    public enum Special{DRAW_ONE,REVERSE,SKIP,FLIP,WILD,WILD_DRAW_TWO_CARDS}
+
+    public enum Rank{ONE,TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE}
+
+    public Card(Rank rank, Colour colour, Special specialCard){
+
+
+
+        this.rank = rank;
+        this.colour = colour;
+        this.special = specialCard;
+
+    }
+
+    public Rank getCardNum(){
+        return this.rank;
+    }
+
+    public Colour getCardColour(){
+        return this.colour;
+    }
+
+    public Special getSpecialType(){
+        return this.special;
+    }
+
+    public String toString(){
+        return "Card Number: " + getCardNum() + ", Card Colour: " + getCardColour() + ", Card Special Type: " + getSpecialType() + "\n";
+    }
+}
