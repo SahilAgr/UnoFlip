a3f51a5142225863d8be0bea039da008caacb24a
Nic <nicolausderikx@cmail.carleton.ca>
2023-10-17 21:54:34 -0400

    - Added addCard method - Added removeCard method - Added findCard method - Modified playCard to use findCard to avoid redundancy - Added removeAllCards method - Removed name-only constructor

--- UnoFlip/src/Player.java
+++ UnoFlip/src/Player.java
@@ -4,11 +4,6 @@ public class Player {
     private String name;
     private ArrayList<Card> hand;
 
-    public Player(String n){
-        name = n;
-        hand = new ArrayList<Card>();
-    }
-
     public Player(String n, ArrayList<Card> h){
         name = n;
         hand = h;
@@ -22,22 +17,37 @@ public class Player {
         name = n;
     }
 
-    public Card playCard(Card.Colour c, Card.Special s, int n){
+    private Card findCard(Card.Colour c, Card.Special s, int n){
         for (Card card : hand){
             if (card.getCardColour() == c && card.getSpecialType() == s && card.getCardNum() == n){
                 hand.remove(card);
                 return card;
             }
         }
-        //This doesn't feel right, I'm going to fix it later.
-        // I just can't remember how you're supposed to handle it right now.
         return null;
+    }
+
+    public Card playCard(Card.Colour c, Card.Special s, int n){
+        Card card = findCard(c, s, n);
+        hand.remove(card);
+        return card;
+    }
+
+    public void addCard(ArrayList<Card> cardList){
+        hand.addAll(cardList);
+    }
+
+    public void removeCard(Card.Colour c, Card.Special s, int n){
+        Card card = findCard(c, s, n);
+        hand.remove(card);
     }
 
     public ArrayList<Card> getHand(){
         return hand;
     }
 
-
+    public void removeAllCards(){
+        hand = new ArrayList<Card>();
+    }
 
 }
