5bab82968efef79f55fd7d152cf2a6ad254da6eb
Nic <nicolausderikx@cmail.carleton.ca>
2023-10-19 18:19:02 -0400

    - Fixed Index Out Of Bounds bug - Fixed some formatting

--- UnoFlip/src/UnoDeck.java
+++ UnoFlip/src/UnoDeck.java
@@ -15,38 +15,33 @@ public class UnoDeck {
         this.cards = new ArrayList<Card>();
 
         // rank 1-9 with color red,yellow,green,blue. NO BLACK, 36 cards
-    for (Card.Colour colour:Card.Colour.values()) {
-        if (colour != Card.Colour.BLACK) {
-            for (Card.Rank rank : Card.Rank.values()) {
-                Card Number_pair = new Card(rank, colour,null);
-                cards.add(Number_pair); // 36 cards in deck
-                cards.add(Number_pair); // 72 cards in deck
-                //System.out.println("ben:" + Number_pair);
+        for (Card.Colour colour:Card.Colour.values()) {
+            if (colour != Card.Colour.BLACK) {
+                for (Card.Rank rank : Card.Rank.values()) {
+                    Card Number_pair = new Card(rank, colour,null);
+                    cards.add(Number_pair); // 36 cards in deck
+                    cards.add(Number_pair); // 72 cards in deck
+                    //System.out.println("ben:" + Number_pair);
+                }
+            // NO black,no wild, each special with red,yellow,green,blue,16 cards
+                for (Card.Special special: Card.Special.values()){
+                    if(special != Card.Special.WILD && special != Card.Special.WILD_DRAW_TWO_CARDS){
+                        Card special_pair = new Card(null,colour,special);
+                        cards.add(special_pair);//16 cards in deck
+                        cards.add(special_pair);//32 cards in deck
+                    }
+                }
+
+
             }
-        // NO black,no wild, each special with red,yellow,green,blue,16 cards
-            for (Card.Special special: Card.Special.values()){
-                if(special != Card.Special.WILD && special != Card.Special.WILD_DRAW_TWO_CARDS){
-                    Card special_pair = new Card(null,colour,special);
-                    cards.add(special_pair);//16 cards in deck
-                    cards.add(special_pair);//32 cards in deck
+            //add the wild card
+            else {
+                for (int i=0;i<4;i++) {
+                    cards.add(new Card(null, Card.Colour.BLACK, Card.Special.WILD));
+                    cards.add(new Card(null, Card.Colour.BLACK, Card.Special.WILD_DRAW_TWO_CARDS));
                 }
             }
-
-
         }
-        //add the wild card
-        else{
-            for(int i=0;i<4;i++){
-                cards.add(new Card(null, Card.Colour.BLACK, Card.Special.WILD));
-                cards.add(new Card(null, Card.Colour.BLACK, Card.Special.WILD_DRAW_TWO_CARDS));
-            }
-        }
-    }
-
-
-
-
-
         Collections.shuffle(cards);
     }
 
@@ -73,10 +68,10 @@ public class UnoDeck {
     public ArrayList<Card> getNCards(int nCards){
         ArrayList<Card> cardArray = new ArrayList<Card>(nCards);
         Random rand = new Random();
-        int upperbound = 112;
-        int int_random = rand.nextInt(upperbound);
+        int int_random;
 
         for(int i = 0; i < nCards; i++){
+            int_random = rand.nextInt(cards.size());
             cardArray.add(cards.get(int_random));
             cards.remove(int_random);
         }
@@ -90,7 +85,7 @@ public class UnoDeck {
 
         System.out.println(unoDeck.getDeckCards());
         //System.out.println(unoDeck.getSevenCards());
-        System.out.println(unoDeck.getNCards(12));
+        System.out.println(unoDeck.getNCards(50));
         System.out.println("the deck has total of : "+ a + " cards");
         int b = unoDeck.cards.size();
         System.out.println("the deck now has:"+b + " cards");
